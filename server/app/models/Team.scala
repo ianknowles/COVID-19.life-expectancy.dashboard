4df634ad52daf74584e6a0615b93409a170957ea
ianknowles <ian@imknowles.co.uk>
2021-08-06 13:41:46 +0100

    update bio for the project

--- server/app/models/Team.scala
+++ server/app/models/Team.scala
@@ -7,11 +7,11 @@ object Team {
 		TeamMember(
 			name = "Ian Knowles",
 			job = "DevOp",
-			desc = "Ian is a data engineer with an academic background and wide-ranging experience in industry. He has " +
-				"development experience in most major languages and has worked on projects that range from embedded device " +
-				"firmware and applications, to desktop and mobile applications, and on to full stack web server development " +
-				"and operations.\n" +
-				"He developed the website backend, frontend, and data visualisations.",
+			desc = "Ian is the LCDS data engineer with an academic background and wide-ranging experience in industry. He " +
+				"has development experience in most major languages and has worked on projects that range from embedded " +
+				"device firmware and applications, to desktop and mobile applications, and on to full stack web server " +
+				"development and operations.\n" +
+				"He developed the website backend, frontend, and converted the visualisations to web formats.",
 			imageURL = routes.Assets.versioned("images/staff/ian.jpg"),
 			links = List(Link(icon = "github", url = "https://github.com/ianknowles", text = "ianknowles"))
 		)
